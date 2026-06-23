3f8c5d66f1e5dfba87eb09f0e9e11633e6c2ac91
saikat <saikat@gmail.com>
2026-06-23 23:33:55 +0530

    landing page done with making input of pickup and drop location

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import HomePage from "./pages/homePage.jsx"
 import UserSignupAndLogin from "./pages/UserSignupAndLogin.jsx"
 import CaptainSignupAndLogin from "./pages/CaptainSignupAndLogin.jsx"
+import LandingPage from "./pages/landingPage.jsx"
 
 import {Routes, Route} from "react-router-dom"
 const app = ()=>{
@@ -10,6 +11,7 @@ const app = ()=>{
  <Route path="/" element={<HomePage/>}/>
 <Route path="/user/account" element={<UserSignupAndLogin />} />
 <Route path="/captain/account" element={<CaptainSignupAndLogin/>}/>
+<Route path="/landingpage" element={<LandingPage/>}/>
     </Routes>
 
 
